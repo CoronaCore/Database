30ddae475f293cd1f77d2949c1f4dd52bd4d9bfa
Salja <salja8840@gmail.com>
2013-12-10 12:14:27 +0100

    [s0008] Fix bad backport for raf system need first by core done
    Signed-off-by: Salja <salja8840@gmail.com>

--- _develop/201_updatepack_mangos.sql
+++ _develop/201_updatepack_mangos.sql
@@ -16,3 +16,6 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
+
+-- Fix bad backport for raf system need first by core done
+DELETE FROM `playercreateinfo_spell` WHERE `Spell` = 45927;
