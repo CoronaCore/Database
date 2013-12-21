bce0bbf37ace428630a6cb4cdb26a8924207db0c
Salja <salja8840@gmail.com>
2013-12-21 17:28:13 +0100

    Update 2201_corepatch_mangos.sql" for next Release
    Signed-off-by: Salja <salja8840@gmail.com>

--- _develop/201_corepatch_mangos.sql
+++ _develop/201_corepatch_mangos.sql
@@ -16,3 +16,18 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
+
+-- s2055_12579_01_mangos_gameobject_template.sql
+ALTER TABLE db_version CHANGE COLUMN required_s2049_12540_01_mangos_command required_s2055_12579_01_mangos_gameobject_template bit;
+
+ALTER TABLE `gameobject_template` ADD `AIName` char(64) NOT NULL DEFAULT '' AFTER `maxgold`;
+
+-- s2055_12579_02_mangos_game_weather.sql
+ALTER TABLE db_version CHANGE COLUMN required_s2055_12579_01_mangos_gameobject_template required_s2055_12579_02_mangos_game_weather bit;
+
+ALTER TABLE `game_weather` ADD `ScriptName` varchar(64) NOT NULL DEFAULT '' AFTER `winter_storm_chance`;
+
+-- s2055_12579_03_mangos_quest_template.sql
+ALTER TABLE db_version CHANGE COLUMN  required_s2055_12579_02_mangos_game_weather required_s2055_12579_03_mangos_quest_template bit;
+
+ALTER TABLE `quest_template` ADD COLUMN `MaxLevel` tinyint(3) unsigned NOT NULL DEFAULT '0' AFTER `MinLevel`;
