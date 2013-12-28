16297f44d96350a374e2ba46d6ad9f21e7389f9d
Salja <salja8840@gmail.com>
2013-12-28 23:22:28 +0100

    [s0012] Some Sunwell Fixes need to test some more will come soon =)
    Signed-off-by: Salja <salja8840@gmail.com>

--- _develop/201_updatepack_mangos.sql
+++ _develop/201_updatepack_mangos.sql
@@ -20,6 +20,36 @@
 -- Fix bad backport for raf system need first by core done
 DELETE FROM `playercreateinfo_spell` WHERE `Spell` = 45927;
 
--- Fix Spell 46707
-DELETE FROM `spell_script_target` WHERE `entry` = 46707;
-INSERT INTO `spell_script_target` VALUES (46707, 1, 25653, 0);
+-- Sunwell Fixes Start
+DELETE FROM `creature_template`_addon WHERE entry=24895;
+
+DELETE FROM `spell_script_target` WHERE `entry` IN (44844, 44884, 45666, 44845, 46609, 46610, 46637, 46650, 46652, 46638, 45388, 45389, 45714, 46707);
+INSERT INTO `spell_script_target` VALUES
+(44844,1,24895,0),
+(44884,1,24895,0),
+(45666,1,25315,0),
+(44845,1,24892,0),
+(44845,1,24891,0),
+(44845,1,24850,0),
+(45388,1,25038,0),
+(45389,1,25265,0),
+(45714,1,25038,0),
+(46609,0,19871,0),
+(46637,1,19871,0),
+(46650,1,23472,0),
+(46707,1,25653,0),
+(46652,0,188075,0),
+(46638,0,188119,0),
+(46610,0,188119,0);
+
+DELETE FROM `spell_target_position` WHERE `id` IN (46020, 46019);
+INSERT INTO `spell_target_position` VALUES
+(46020,580,1690.339,942.1176,53.07742,0),
+(46019,580,1696.196,951.1885,-74.55846,0);
+
+DELETE FROM `dbscripts_on_spell` WHERE `id` IN (46650, 46609);
+INSERT INTO `dbscripts_on_spell` (`id`,`command`,`datalong`,`data_flags`,`comments`) VALUES
+(46609,15,46610,8,'Cast Freeze on Ice Barrier'),
+(46650,15,46652,8,'Cast Open Brutallus Back Door on Fire Barrier');
+
+-- Sunwell Fixes End
