4f4693afbf3fb49e97cb75de5a16ce0e2942336b
Salja <salja8840@gmail.com>
2014-01-29 14:53:10 +0100

    [s0017] Update Copyright
    Signed-off-by: Salja <salja8840@gmail.com>

--- _full_db/locales_gossip_menu_option.sql
+++ _full_db/locales_gossip_menu_option.sql
@@ -1,6 +1,5 @@
 --
--- Copyright (C) 2005-2013 MaNGOS <http://getmangos.com/>
--- Copyright (C) 2009-2013 MaNGOSOne <https://github.com/mangosone>
+-- Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
