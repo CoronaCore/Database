10eb9e8e63a164fc90610d6d6310e879873a1de4
Salja <salja8840@gmail.com>
2014-10-08 12:26:41 +0200

    Fix Quests 667, 1090, 5713 Thanks Xfurry
    Signed-off-by: Salja <salja8840@gmail.com>

--- _full_db/conditions.sql
+++ _full_db/conditions.sql
@@ -41,7 +41,7 @@ CREATE TABLE `conditions` (
   `value2` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'data field two for the condition',
   PRIMARY KEY (`condition_entry`),
   UNIQUE KEY `unique_conditions` (`type`,`value1`,`value2`)
-) ENGINE=MyISAM AUTO_INCREMENT=532 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Condition System';
+) ENGINE=MyISAM AUTO_INCREMENT=534 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Condition System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -578,7 +578,9 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES
 (528,9,10040,0),
 (529,-1,528,527),
 (530,9,10041,0),
-(531,-1,530,527);
+(531,-1,530,527),
+(532,36,0,60),
+(533,36,3,0);
 /*!40000 ALTER TABLE `conditions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
