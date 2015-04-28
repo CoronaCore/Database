719ac759bb32c9654728c8b934eb0c6f44ad7588
Salja <salja8840@gmail.com>
2015-04-28 15:16:13 +0200

    Fix Quest 10040
    Signed-off-by: Salja <salja8840@gmail.com>

--- _full_db/conditions.sql
+++ _full_db/conditions.sql
@@ -41,7 +41,7 @@ CREATE TABLE `conditions` (
   `value2` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'data field two for the condition',
   PRIMARY KEY (`condition_entry`),
   UNIQUE KEY `unique_conditions` (`type`,`value1`,`value2`)
-) ENGINE=MyISAM AUTO_INCREMENT=589 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Condition System';
+) ENGINE=MyISAM AUTO_INCREMENT=590 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Condition System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -635,7 +635,8 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES
 (585,8,9705,0),
 (586,8,8350,0),
 (587,-1,586,585),
-(588,9,9437,0);
+(588,9,9437,0),
+(589,1,32756,0);
 /*!40000 ALTER TABLE `conditions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
