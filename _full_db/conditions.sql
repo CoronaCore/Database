840e2bb3f63df605a209c4ba2099714d09fc1e7d
Salja <salja8840@gmail.com>
2015-04-29 18:02:43 +0200

    Fix gossip for creature 20678
    Signed-off-by: Salja <salja8840@gmail.com>

--- _full_db/conditions.sql
+++ _full_db/conditions.sql
@@ -41,7 +41,7 @@ CREATE TABLE `conditions` (
   `value2` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'data field two for the condition',
   PRIMARY KEY (`condition_entry`),
   UNIQUE KEY `unique_conditions` (`type`,`value1`,`value2`)
-) ENGINE=MyISAM AUTO_INCREMENT=591 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Condition System';
+) ENGINE=MyISAM AUTO_INCREMENT=592 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Condition System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -637,7 +637,8 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES
 (587,-1,586,585),
 (588,9,9437,0),
 (589,1,32756,0),
-(590,12,8,0);
+(590,12,8,0),
+(591,9,10368,0);
 /*!40000 ALTER TABLE `conditions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
